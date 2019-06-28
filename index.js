0133adf38de938d514f0c8d669feabefb134bc65
mroger <rene.mroger@gmail.com>
2019-06-28 20:52:05 +0000

    index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 
-function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
-}
-
+const sum = function(fromN, toN) {
+  let add = 0;
+  if (fromN > toN) {
+    return add;
+  } else {
+    add = fromN;
+  }
+  return add + sum(fromN + 1, toN);
+};
 module.exports = sum;
